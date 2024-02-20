8122229b6349aee7ad69748a12a88d2a493afc75
temirbaevabermet <temirbaevabermet4@gmail.com>
2024-02-21 00:06:20 +0600

    first commit

--- scripts/scpript.js
+++ scripts/scpript.js
@@ -2,10 +2,9 @@ const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello World!";
 
 let myVariable = 'mmm';
-
 alert(myVariable);
 const imgElement = document.querySelector("img");
-const img1URL = "images\Ala-Too_International_University_Seal.png";
+const img1URL = "images/alatoo-logo.png";
 const img2URL = "images\nature-scenery.jpg";
 function changePhoto(event){
     let current = imgElement.getAttribute("src");
